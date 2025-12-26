e5f62452df3b4c212d178cbeee2fded15f0fb975
Krittaya-Th <krittayaclf9028@gmail.com>
2025-12-26 17:34:16 +0700

    lab10_2_completed

--- lab10_2.cpp
+++ lab10_2.cpp
@@ -8,9 +8,10 @@ int main (){
 	ofstream dest;
 	source.open("cheerbook.txt") ;
 	dest.open("cheerbook_copy.txt");
-	
-	//กรอก Code เฉพาะส่วนนี้ ให้สอดคล้องกับโจทย์และ Code ส่วนอื่น ๆ 
-	//โดยห้ามแก้ไข Code ในส่วนอื่น ๆ
+	string text;
+	while(getline(source,text)){
+		cout << text<<endl;
+	}
 
     source.close();
     dest.close();
